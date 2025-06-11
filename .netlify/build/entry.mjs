1ed85ae3c54010f16c81164c262baf6617fd180e
MieVenoe00 <mieven3009@gmail.com>
2025-06-11 23:10:57 +0200

    new dist

---
+++ .netlify/build/entry.mjs
@@ -0,0 +1,30 @@
+import { renderers } from './renderers.mjs';
+import { s as serverEntrypointModule } from './chunks/_@astrojs-ssr-adapter_CvSoi7hX.mjs';
+import { manifest } from './manifest_Cx7b_jnQ.mjs';
+import { createExports } from '@astrojs/netlify/ssr-function.js';
+
+const serverIslandMap = new Map();;
+
+const _page0 = () => import('./pages/index.astro.mjs');
+const pageMap = new Map([
+    ["src/pages/index.astro", _page0]
+]);
+
+const _manifest = Object.assign(manifest, {
+    pageMap,
+    serverIslandMap,
+    renderers,
+    actions: () => import('./_noop-actions.mjs'),
+    middleware: () => import('./_noop-middleware.mjs')
+});
+const _args = {
+    "middlewareSecret": "5254ee3e-f15b-43ba-a502-a56dbc586b66"
+};
+const _exports = createExports(_manifest, _args);
+const __astrojsSsrVirtualEntry = _exports.default;
+const _start = 'start';
+if (_start in serverEntrypointModule) {
+	serverEntrypointModule[_start](_manifest, _args);
+}
+
+export { __astrojsSsrVirtualEntry as default, pageMap };
